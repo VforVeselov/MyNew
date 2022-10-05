0ea47fa5e87a3451f63623d29692b65cc15d6792
Nikita <nikiteveselovu@gmail.com>
2022-10-05 23:32:09 +0300

    ПЕреписал логику работы с сообщениями и ответами

--- src/main/java/BotClass.java
+++ src/main/java/BotClass.java
@@ -11,13 +11,18 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 @Slf4j
-public class MyAmazingBot extends TelegramLongPollingBot {
+public class BotClass  extends TelegramLongPollingBot {
+
     private final DataController dataController = new DataController();
     private Random random = new Random();
+    private SendMessage message = new SendMessage();
+    private SendPhoto sendPhoto = new SendPhoto();
+
 
     @Override
     public String getBotUsername() {
@@ -32,11 +37,11 @@ public class MyAmazingBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
+
+        // Отлавливаем все сообщения
         if (update.hasMessage() && update.getMessage().hasText()) {
-            // Set variables
-            SendMessage message = new SendMessage();
-            SendPhoto sendPhoto = new SendPhoto();
             message.setChatId(update.getMessage().getChatId().toString());
+            // start
             if (update.getMessage().getText().equals("/start")) {
                 message.setText("Привет! \n Этот бот поможет тебе выучить асаны в форме теста \n жмякни /memorize и поехали!");
                 try {
@@ -45,22 +50,20 @@ public class MyAmazingBot extends TelegramLongPollingBot {
                     throw new RuntimeException(e);
                 }
             }
-
+            // memorize
             if (update.getMessage().getText().equals("/memorize")) {
-                    System.out.println("memorize");
+                log.info("User {} запустил memorize",update.getMessage().getFrom().getUserName());
                 QuizClass quizClass = new QuizClass();
                 quizClass.sendQuestion(sendPhoto,dataController,update.getMessage().getChatId());
-
-                try {
-                    execute(sendPhoto);
-                    //execute(message);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
+                    try {
+                        execute(sendPhoto);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
             }
-
+            // practice
             if (update.getMessage().getText().equals("/practice")) {
-                    System.out.println("practice");
+                log.trace("User {} запустил practice",update.getMessage().getFrom().getUserName());
                 PracticeController practice = new PracticeController();
                 try {
                     LinkedList<Asana> practiceList = practice.getPractice(new Integer[]{3, 12, 6, 13, 14, 11, 9, 15, 14, 11, 9, 15, 14, 11, 9, 13, 6, 12, 3});
@@ -70,77 +73,22 @@ public class MyAmazingBot extends TelegramLongPollingBot {
                 }
             }
 
-        } else if (update.hasCallbackQuery()) {
-
-            if (!update.getCallbackQuery().getData().equals("-")) { // правильный ответ
-                if (update.getCallbackQuery().getData().equals("yestomorequiz")) {
-                    log.trace("aaa");
-                    //System.out.println("ssssss");
-                    SendMessage message = new SendMessage();
-                    SendPhoto sendPhoto = new SendPhoto();
-                    message.setChatId(update.getCallbackQuery().getMessage().getChatId().toString());
-
-                    QuizClass quizClass = new QuizClass();
-                    quizClass.sendQuestion(sendPhoto,dataController,update.getCallbackQuery().getMessage().getChatId());
-
+        }
+        // Отлавливаем коллбеки из клаиауры
+        else if (update.hasCallbackQuery()) {
+            message.setChatId(update.getCallbackQuery().getMessage().getChatId().toString());
+            // если нужно продолжить квиз
+            if (update.getCallbackQuery().getData().equals("yestomorequiz")) {
+                QuizClass quizClass = new QuizClass();
+                quizClass.sendQuestion(sendPhoto,dataController,update.getCallbackQuery().getMessage().getChatId());
                     try {
                         execute(sendPhoto);
-                        //execute(message);
                     } catch (TelegramApiException e) {
                         e.printStackTrace();
                     }
-                }
-
-                SendMessage newMessage = new SendMessage();
-                newMessage.setChatId(update.getCallbackQuery().getMessage().getChatId());
-                newMessage.setText(Settings.rightAnswer.get(random.nextInt(Settings.rightAnswer.size()))); // один из вариантов верного ответа
-
-                SendMessage infoMessage = new SendMessage();
-                infoMessage.setChatId(update.getCallbackQuery().getMessage().getChatId());
-                try {
-                    Integer asanaId = null;
-                        try {
-                            asanaId = Integer.parseInt(update.getCallbackQuery().getData());
-                            log.trace(String.valueOf(asanaId));
-                        } catch (NumberFormatException e) {
-                            e.printStackTrace();
-                        }
-                    infoMessage.setText(dataController.getAsaaInfo(asanaId));
-
-                    Message response = execute(newMessage);
-                    // удаляем к херам
-                    this.deleteMessage(response, update, 1000);
-
-                    Message info = execute(infoMessage);
-                    this.deleteMessage(info, update, 7000);
-
-                    Message quizMessage = update.getCallbackQuery().getMessage();
-                    deleteMessage(quizMessage, update, 7000);
-                    // что-то не работает ((((
-                    deleteMessage(
-                            execute(
-                                    SendMessage.builder()
-                                    .text("Еще?")
-                                    .chatId(update.getCallbackQuery().getMessage().getChatId())
-                                            .replyMarkup(InlineKeyboardMarkup.builder()
-                                                    .keyboardRow(List.of(InlineKeyboardButton.builder()
-                                                            .text("Yes")
-                                                            .callbackData("yestomorequiz")
-                                                            .build()
-                                                    )).build()
-                                            )
-                                    .build()
-                            ),
-                            update, 7000);
-
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-
-            }  else {
+            }
+            // если НЕправильный ответ
+            if (update.getCallbackQuery().getData().equals("-")) {
                 log.trace("ответ неверный");
                 SendMessage newMessage = new SendMessage();
                 newMessage.setChatId(update.getCallbackQuery().getMessage().getChatId());
@@ -152,26 +100,53 @@ public class MyAmazingBot extends TelegramLongPollingBot {
                     e.printStackTrace();
                 }
             }
+            // Если ответ правильный ra{id}
+            if (update.getCallbackQuery().getData().startsWith("ra")) {
+                message.setChatId(update.getCallbackQuery().getMessage().getChatId());
+                message.setText(Settings.rightAnswer.get(random.nextInt(Settings.rightAnswer.size()))); // один из вариантов верного ответа
 
+                SendMessage infoMessage = new SendMessage();
+                infoMessage.setChatId(update.getCallbackQuery().getMessage().getChatId());
+                try {
+                    Integer asanaId = null;
+                    asanaId = Integer.parseInt(update.getCallbackQuery().getData().substring(2)); //обрезаем ra
+                    infoMessage.setText(dataController.getAsaaInfo(asanaId));
+
+                    Message response = execute(message);
+                    // удаляем к херам
+                    this.deleteMessage(response, update, 1000);
+
+                    Message info = execute(infoMessage);
+                    this.deleteMessage(info, update, 7000);
+
+                    Message quizMessage = update.getCallbackQuery().getMessage();
+                    deleteMessage(quizMessage, update, 7000);
+
+                    deleteMessage(
+                            execute(
+                                    SendMessage.builder()
+                                            .text("Еще?")
+                                            .chatId(update.getCallbackQuery().getMessage().getChatId())
+                                            .replyMarkup(InlineKeyboardMarkup.builder()
+                                                    .keyboardRow(List.of(InlineKeyboardButton.builder()
+                                                            .text("Да-а-а")
+                                                            .callbackData("yestomorequiz")
+                                                            .build()
+                                                    )).build()
+                                            )
+                                            .build()
+                            ),
+                            update, 7000);
+
+                } catch (TelegramApiException | NumberFormatException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
 
 
-    }
-
-    private void deleteMessage(Message response, Update update, int time) {
-        Thread run = new Thread(() -> {
-            try {
-                Thread.sleep(time);
-                DeleteMessage del = new DeleteMessage();
-                del.setChatId(update.getCallbackQuery().getMessage().getChatId());
-                del.setMessageId(response.getMessageId());
-                execute(del);
-            } catch (InterruptedException | TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
-
-        });
-        run.start();
     }
 
     private void startPractice(LinkedList<Asana> asanaLinkedList,Update update, int time) {
@@ -190,6 +165,22 @@ public class MyAmazingBot extends TelegramLongPollingBot {
                     execute(practiceMessage);
                     Thread.sleep(time);
                 }
+            } catch (InterruptedException | TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+        run.start();
+    }
+
+    private void deleteMessage(Message response, Update update, int time) {
+        Thread run = new Thread(() -> {
+            try {
+                Thread.sleep(time);
+                DeleteMessage del = new DeleteMessage();
+                del.setChatId(update.getCallbackQuery().getMessage().getChatId());
+                del.setMessageId(response.getMessageId());
+                execute(del);
             } catch (InterruptedException | TelegramApiException e) {
                 throw new RuntimeException(e);
             }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new MyAmazingBot());
+            botsApi.registerBot(new BotClass());
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
