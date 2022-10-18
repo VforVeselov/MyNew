838e867022f960486d378b7235ad01796065527e
VforVeselov <nikiteveselovu@gmail.com>
2022-10-18 07:16:11 +0300

    теперь токен не хранится в гите )

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -9,9 +9,10 @@ import java.util.List;
 @Value
 public class Settings {
     public static String botName = "Bodhi";
-    // export botToken = {YOUR_BOT_TOKEN}
-    //public static String botToken = System.getenv("BOT_TOKEN");
-    public static String token = "1591576699:AAF_E5hPn_BLQJ6K4WC0CnxSC8-3iGnjmSw";
+    // ./bashrc
+    // export BOT_TOKEN={YOUR_BOT_TOKEN}
+    //
+    public static String token = System.getenv("BOT_TOKEN").substring(10);
     public static List<String> rightAnswer = Arrays.asList("Круто!", "Молодец", "Умница");
     public static List<String> wrongMessage = Arrays.asList("Не-а", "Подумай еще", "Не верно", "Ну нет");
 }
